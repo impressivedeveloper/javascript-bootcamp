fbab64785f7cd0fa699a5f61fefd9f8b318a8235
impressivedeveloper <livingimpressiveforever@gmail.com>
2023-09-01 10:17:28 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,98 @@
+/*
+let js = "Amazing";
+console.log(js);
+
+let firstName = "BAB";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let person = "Babu";
+
+let myFirstJob = "Programmer";
+let mySecondJob = "Teacher";
+
+// JavaScript Data Types
+
+let javascripIsFun = true;
+console.log(javascripIsFun);
+
+console.log(typeof true);
+javascripIsFun = "YES";
+console.log(typeof javascripIsFun);
+
+let year;
+console.log(year);
+year = 2023;
+console.log(year);
+
+*/
+
+// Var, let & const
+
+var job = "Programmer";
+job = "Teacher";
+
+lastName = "Nahid";
+console.log(lastName);
+
+// Math Operators
+
+const now = 2037;
+const ageNahid = now - 1991;
+const ageBarbara = now - 2018;
+
+console.log(ageNahid, ageBarbara);
+
+console.log(ageNahid * 2, ageBarbara / 2, 3 ** 3);
+
+const first_Name = 'Nahid';
+const last_Name = 'Islam';
+
+console.log(first_Name + ' ' + last_Name);
+
+
+//Assignment Operators
+let x = 10 + 5;// 15
+x += 10; //25
+x *= 4; //100
+x++; // x = x + 1 
+x--; //
+x--;
+
+console.log(x);
+
+
+//Comparison Operator
+console.log(ageNahid > ageBarbara);
+console.log(ageNahid >= 101);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
